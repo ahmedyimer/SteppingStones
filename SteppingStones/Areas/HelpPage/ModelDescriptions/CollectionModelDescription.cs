cde33ecf099c28cf123f99975954d3e11b39a9e5
Ahmed Yimer <ahmed.yimer@gmail.com>
2019-06-14 23:14:58 -0500

    Add project files.

---
+++ SteppingStones/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SteppingStones.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
